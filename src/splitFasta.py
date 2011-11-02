501fac1a11a91329f97951b83f7465a639fd074e
Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
2011-11-02 18:02:15 +0000

    Modified the splitFasta program to dump and count unrecognised sequences.

--- src/splitFasta.py
+++ src/splitFasta.py
@@ -3,38 +3,56 @@
 from Bio import SeqIO
 import sys
 
-# Read the library file.
-handle = open(sys.argv[2], "r")
-library = []
-line = handle.readline()
-while line :
+def addToLibrary(library, line) :
+    """
+    """
+
     library.append(line.split())
     library[-1].append(open(library[-1][0] + ".txt", "w"))
     library[-1].append(open(library[-1][0] + "_counted.txt", "w"))
     library[-1].append(0)
     library[-1].append({})
+#addToLibrary
+
+def writeOutput(entry, description, sequence) :
+    """
+    """
+
+    entry[3].write(">%s\n" % description)
+    entry[3].write("%s\n" % sequence)
+    entry[5] += 1
+    if entry[6].has_key(sequence) :
+        entry[6][sequence][1] += 1
+    else :
+        entry[6][sequence] = [description, 1]
+#writeOutput
+
+# Read the library file.
+handle = open(sys.argv[2], "r")
+library = []
+line = handle.readline()
+while line :
+    addToLibrary(library, line)
     line = handle.readline()
 #while
 handle.close()
+addToLibrary(library, "Unrecognised _INVALID_ _INVALID_")
 
 # Split the fasta file.
 total = 0
 recognised = 0
 handle = open(sys.argv[1], "r")
 for record in SeqIO.parse(handle, "fasta") :
+    hit = False
     for entry in library :
         if entry[1] in record.seq :
             flanks = str(record.seq).split(entry[1])
-            newSeq = flanks[0] + entry[2] + flanks[1]
-            entry[3].write(">%s\n" % record.description)
-            entry[3].write("%s\n" % newSeq)
-            entry[5] += 1
-            if entry[6].has_key(newSeq) :
-                entry[6][newSeq][1] += 1
-            else :
-                entry[6][newSeq] = [record.description, 1]
+            writeOutput(entry, record.description, flanks[0] + entry[2] + flanks[1])
             recognised += 1
+            hit = True
         #if
+        if entry[1] == "_INVALID_" and not hit :
+            writeOutput(entry, record.description, str(record.seq))
     #for
     total += 1
 #for
