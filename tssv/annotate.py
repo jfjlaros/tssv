dc4b0a33dc40bbad1b9c3182e44d9c8980e4c05f
Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
2013-01-16 16:50:29 +0000

    Added an annotation script that uses Mutalyzer to analyse the alleles.

---
+++ tssv/annotate.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+"""
+Convert a csv files containing alleles and counts to HGVS descriptions of
+alleles and single variants. Also report statistics about the variant types.
+
+
+The input file is typically one of the output files of tssv.
+"""
+
+import sys
+import numpy
+import argparse
+import collections
+from os import path
+from suds import client
+
+import tssv
+
+URL = "https://mutalyzer.nl/services/?wsdl"
+
+def writeTable(data, title, reportHandle):
+    """
+    Write a table to a file.
+
+    @arg data: Dictionary containing counts per type.
+    @type data: dict
+    @arg title: Name of the first column.
+    @type title: str
+    @arg reportHandle: Open writeable handle to the report file.
+    @type reportHandle: stream
+    """
+    reportHandle.write("%s\ttotal\tforward\treverse\n" % title)
+
+    for i in data:
+        reportHandle.write("%s\t%i\t%i\t%i\n" % tuple([i] + list(data[i])))
+
+    reportHandle.write("\n")
+#writeTable
+
+def annotate(allelesHandle, reference, reportHandle):
+    """
+    Convert a csv files containing alleles and counts to HGVS descriptions of
+    alleles, single variants and variant types.
+
+    @arg allelesHandle: Open handle to the alleles file.
+    @type allelesHandle: stream
+    @arg reference: The reference sequence.
+    @type reference: str
+    @arg reportHandle: Open writeable handle to the report file.
+    @type reportHandle: stream
+    """
+    alleles = collections.defaultdict(lambda: numpy.array([0, 0, 0]))
+    rawVars = collections.defaultdict(lambda: numpy.array([0, 0, 0]))
+    classification = collections.defaultdict(lambda: numpy.array([0, 0, 0]))
+    service = client.Client(URL).service
+
+    data = map(lambda x: x.strip().split('\t'), allelesHandle.readlines()[1:])
+    for i in data:
+        alleleDescription = service.descriptionExtract(reference=reference,
+            observed=i[0])
+        encountered = map(int, (i[1:]))
+
+        alleles[alleleDescription.description] += encountered
+        for j in alleleDescription.allele:
+            for k in j[1]:
+                rawVars[k.hgvs] += encountered
+                classification[k.type] += encountered
+            #for
+    #for
+
+    writeTable(alleles, "allele", reportHandle)
+    writeTable(rawVars, "variant", reportHandle)
+    writeTable(classification, "class", reportHandle)
+#annotate
+
+def main():
+    """
+    Main entry point.
+    """
+    usage = __doc__.split("\n\n\n")
+    parser = argparse.ArgumentParser(description=usage[0], epilog=usage[1],
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument("alleles", metavar="alleles",
+        type=argparse.FileType("r"), help="the alleles file")
+    parser.add_argument("reference", metavar="reference", type=str,
+        help="sequence of the reference allele")
+    parser.add_argument("-r", dest="report", type=argparse.FileType("w"),
+        default=sys.stdout, help="name of the report file")
+
+    args = parser.parse_args()
+
+    try:
+        annotate(args.alleles, args.reference, args.report)
+    except OSError, error:
+        parser.error(error)
+#main
+
+if __name__ == "__main__":
+    main()
