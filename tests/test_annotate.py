2ffe1192d44fad4f21191934c90fb4f7b8b2536c
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2016-10-13 20:41:18 +0200

    Added unit tests for annotate CLI.

---
+++ tests/test_annotate.py
@@ -0,0 +1,29 @@
+"""
+Tests for the annotation CLI.
+"""
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+from future.builtins import str, zip
+
+from hashlib import md5
+from io import StringIO
+
+from tssv import annotate
+
+
+def md5_check(data, md5sum):
+    return md5(data.encode()).hexdigest() == md5sum
+
+
+class TestAnnotation(object):
+    """
+    Test the annotation CLI.
+    """
+    def setup(self):
+        self._input = open('data/m1_newalleles.csv')
+        self._output = StringIO()
+
+    def test_with_dna(self):
+        annotate.annotate(self._input, 'TCCGTCCCATGCATGC', self._output, 0)
+        assert md5_check(
+            self._output.getvalue(), '596241e84c6d20a4155034236c234c51')
